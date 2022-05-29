0080120d7f97b9b55cf72d83c97319de4955c1fc
Javad Zanguei <jzanguei@gmail.com>
2022-05-29 15:06:56 +0330

    Upload Todo exercise by Vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <nav class="navbar bg-light">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Todo</span>
+      </div>
+    </nav>
+    <h1 class="text-lg text-center text-light bg-dark py-5">
+    Todo App
+    </h1>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style>
+
+</style>
+
+<script>
+  export default {
+
+  }
+</script>
